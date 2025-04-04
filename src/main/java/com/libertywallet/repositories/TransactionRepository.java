eac34497015a7e831d1a3fe29e9c059f88cc4ca7
Marmadis <askarryspaev04@gmail.com>
2025-04-04 22:29:23 +0500

    Добавление ARIMA

--- src/main/java/com/libertywallet/repositories/TransactionRepository.java
+++ src/main/java/com/libertywallet/repositories/TransactionRepository.java
@@ -3,12 +3,28 @@ package com.libertywallet.repositories;
 
 import com.libertywallet.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
+import java.time.YearMonth;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
     List<Transaction> findByUserId(Long userId);
+
+    @Query(value = """
+        SELECT TO_CHAR(date, 'YYYY-MM') AS month, SUM(amount) AS total
+        FROM transactions
+        WHERE user_id = :userId
+        GROUP BY month
+        ORDER BY month
+    """, nativeQuery = true)
+    List<Object[]> getRawMonthlyExpenses(@Param("userId") Long userId);
+
 
 }
